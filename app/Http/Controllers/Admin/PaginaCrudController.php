d80b9b5afa09dac9e12b9be495134a6fc09bc6b9
jose <josedgregorio07@gmail.com>
2018-05-02 13:57:19 -0600

    Actualizacion de commit

---
+++ app/Http/Controllers/Admin/PaginaCrudController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+
+class PaginaCrudController extends CrudController
+{
+    public function index()
+    {
+		return view('index');
+    }
+}
